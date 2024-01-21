238a53722800f7d52c512be28dfa1b8ea702736b
Arlei F. Farnetani Junior <farnetani@gmail.com>
2024-01-21 12:12:52 -0300

    feat: drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,14 +16,125 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final PageController _pageController = PageController();
   int pageSelected = 0;
+  accountName() => const Text('Arlei F. Farnetani Junior');
+  accountEmail() => const Text('farnetani@gmail.com');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Home Page'),
+          title: const Text('Farsoft Suporte'),
           foregroundColor: Colors.black87,
           backgroundColor: Colors.green[300],
+        ),
+        drawer: Container(
+          width: 250,
+          child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: accountName(),
+                  accountEmail: accountEmail(),
+                  currentAccountPicture: const CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      'A',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                    log('Home');
+                    Navigator.pop(context);
+                    setState(() {
+                      pageSelected = 0;
+                      _pageController.animateToPage(
+                        pageSelected,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.shopping_bag_outlined),
+                  title: const Text('Produtos'),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                    log('Perfil');
+                    Navigator.pop(context);
+                    setState(() {
+                      pageSelected = 1;
+                      _pageController.animateToPage(
+                        pageSelected,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.shopping_cart),
+                  title: const Text('Carrinho de Compras'),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                    log('Perfil');
+                    Navigator.pop(context);
+                    setState(() {
+                      pageSelected = 1;
+                      _pageController.animateToPage(
+                        pageSelected,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text('Perfil'),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                    log('Perfil');
+                    Navigator.pop(context);
+                    setState(() {
+                      pageSelected = 1;
+                      _pageController.animateToPage(
+                        pageSelected,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Configurações'),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                    log('Configurações');
+                    Navigator.pop(context);
+                    setState(() {
+                      pageSelected = 2;
+                      _pageController.animateToPage(
+                        pageSelected,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
         body: PageView(
           controller: _pageController,
